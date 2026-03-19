b669e8cda10532ae961e46ad0fc9c8d65347214f
Yan-Novikov <fattuna@bk.ru>
2026-03-19 16:55:33 +0700

    module2 task 2.1

---
+++ module2/2/2.1/calculator.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int add(double a, double b, double *result);
+int substract(double a, double b, double *result);
+int multiply(double a, double b, double *result);
+int divide(double a, double b, double *result);
+int max(double a, double b, double *result);
+int min(double a, double b, double *result);
+
+#endif
